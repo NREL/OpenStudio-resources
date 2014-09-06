243b815fb68f8b5986061d322cafaee1c7e8f6d3
Andrew Parker <andrew.parker@nrel.gov>
2014-09-06 11:47:07 -0600

    Updates tests for humidity SPMs.  Tries all 3 types.

--- model/simulationtests/humidity_control.rb
+++ model/simulationtests/humidity_control.rb
@@ -25,28 +25,61 @@ model.add_thermostats({"heating_setpoint" => 24,
                       "cooling_setpoint" => 28})
 
 #pick out on of the zone/system pairs and add a humidifier
-zones = model.getThermalZones
-zone = zones[0]
+zones = model.getThermalZones.sort
 
-dehumidify_sch = OpenStudio::Model::ScheduleConstant.new(model)
-dehumidify_sch.setValue(50)
-humidistat = OpenStudio::Model::ZoneControlHumidistat.new(model)
-humidistat.setHumidifyingRelativeHumiditySetpointSchedule(dehumidify_sch)
-zone.setZoneControlHumidistat(humidistat)
+# Try out all 3 different types of humidity setpoint managers
+# on three different airloops in the same model.
+for i in 0..2
+  
+  zone = zones[i]
+  
+  # Add a humidistat at 50% RH to the zone
+  dehumidify_sch = OpenStudio::Model::ScheduleConstant.new(model)
+  dehumidify_sch.setValue(50)
+  humidistat = OpenStudio::Model::ZoneControlHumidistat.new(model)
+  humidistat.setHumidifyingRelativeHumiditySetpointSchedule(dehumidify_sch)
+  zone.setZoneControlHumidistat(humidistat)
+  
+  # Add a humidifier after the gas heating coil and before the fan
+  air_system = zone.airLoopHVAC.get
+  htg_coil = air_system.supplyComponents(OpenStudio::Model::CoilHeatingGas::iddObjectType()).first.to_CoilHeatingGas.get
+  htg_coil_outlet_node = htg_coil.outletModelObject.get.to_Node.get
+  humidifier = OpenStudio::Model::HumidifierSteamElectric.new(model)
+  humidifier.addToNode(htg_coil_outlet_node)
+  humidifier_outlet_node = humidifier.outletModelObject.get.to_Node.get
+  
+  # Try out all 3 different types of humidity setpoint managers
+  # by adding them to the humidifier outlet node.
+  case i
+  when 0
+    spm = OpenStudio::Model::SetpointManagerSingleZoneHumidityMinimum.new(model)
+    spm.addToNode(humidifier_outlet_node)
+  when 1
+    spm = OpenStudio::Model::SetpointManagerMultiZoneHumidityMinimum.new(model)
+    spm.addToNode(humidifier_outlet_node)
+  when 2
+    spm = OpenStudio::Model::SetpointManagerMultiZoneMinimumHumidityAverage.new(model)
+    spm.addToNode(humidifier_outlet_node)
+  end
+  
+end
 
-air_system = zone.airLoopHVAC.get
-fan = air_system.supplyComponents(OpenStudio::Model::FanConstantVolume::iddObjectType()).first.to_FanConstantVolume.get
-node = fan.inletModelObject.get.to_Node.get
-humidifier = OpenStudio::Model::HumidifierSteamElectric.new(model)
-humidifier.addToNode(node)
-node = humidifier.outletModelObject.get.to_Node.get
-spm = OpenStudio::Model::SetpointManagerSingleZoneHumidityMinimum.new(model)
-spm.addToNode(node)
-#spm = OpenStudio::Model::SetpointManagerMultiZoneHumidityMinimum.new(model)
-#spm.addToNode(node)
-#spm = OpenStudio::Model::SetpointManagerMultiZoneMinimumHumidityAverage.new(model)
-#spm.addToNode(node)
-              
+# Request timeseries data for debugging
+=begin
+reporting_frequency = "hourly"
+var_names << "System Node Setpoint Temperature"
+var_names << "System Node Setpoint Minimum Humidity Ratio"
+var_names << "System Node Setpoint Humidity Ratio"
+var_names << "Zone Mean Air Humidity Ratio"
+var_names << "Zone Mean Air Temperature"
+var_names << "Zone Air Relative Humidity"
+var_names << "Humidifier Water Volume Flow Rate"
+var_names.each do |var_name|
+  outputVariable = OpenStudio::Model::OutputVariable.new(var_name,model)
+  outputVariable.setReportingFrequency(reporting_frequency)
+end          
+=end
+           
 #assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions()
 
