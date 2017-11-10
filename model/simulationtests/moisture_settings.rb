c3b98d1ce00ecbf3bd2e97524c59a2fc5d0dc244
Joe Robertson <joseph.robertson@nrel.gov>
2017-11-10 09:26:49 -0700

    Results make sense.

--- model/simulationtests/moisture_settings.rb
+++ model/simulationtests/moisture_settings.rb
@@ -4,8 +4,6 @@ require 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# /c/OpenStudio/OpenStudio/core-build/Products/Debug/openstudio.exe model_tests.rb -n test_moisture_settings_rb
-
 heat_balance_algorithm = model.getHeatBalanceAlgorithm
 heat_balance_algorithm.setAlgorithm("MoisturePenetrationDepthConductionTransferFunction")
 
@@ -16,9 +14,27 @@ model.add_geometry({"length" => 100,
                     "floor_to_floor_height" => 4,
                     "plenum_height" => 1,
                     "perimeter_zone_depth" => 3})
-              
+                    
+#add windows at a 40% window-to-wall ratio
+model.add_windows({"wwr" => 0.4,
+                   "offset" => 1,
+                   "application_type" => "Above Floor"})
+
+#add ASHRAE System type 01, PTAC, Residential
+model.add_hvac({"ashrae_sys_num" => '01'})
+
+#add thermostats
+model.add_thermostats({"heating_setpoint" => 24,
+                       "cooling_setpoint" => 28})
+                       
 #assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions()
+
+#set whole building space type; simplified 90.1-2004 Large Office Whole Building
+model.set_space_type()
+
+#add design days to the model (Chicago)
+model.add_design_days()
 
 #assign material property moisture penetration depth settings to walls
 model.getSurfaces.each do |surface|
