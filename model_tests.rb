3257e85562ba7bb95224b5222d65d2278f8628d5
Joe Robertson <joseph.robertson@nrel.gov>
2018-01-30 10:55:50 -0700

    Stub tests for AdditionalProperties.

--- model_tests.rb
+++ model_tests.rb
@@ -983,6 +983,14 @@ class ModelTests < MiniTest::Unit::TestCase
     result = sim_test('foundation_kiva.rb')
   end
   
+  def test_additional_props_rb
+    result = sim_test('additional_props.rb')
+  end
+  
+  def test_additional_props_osm
+    result = sim_test('additional_props.osm')
+  end
+  
   # intersection tests
 
   def test_intersect_22_osm
