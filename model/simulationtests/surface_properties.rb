e7b8e95e4c329ec0ccec73df8dd154a6264f4696
Julien Marrec <julien.marrec@gmail.com>
2018-02-09 16:18:18 +0100

    Replace most OSMs I added with 2.0.5 instead of 2.0.4
    None of the OS 2.0.4 can be version translated to a newer than 2.0.5 version for some reason

--- model/simulationtests/surface_properties.rb
+++ model/simulationtests/surface_properties.rb
@@ -16,19 +16,19 @@ model.add_geometry({"length" => 100,
 model.add_windows({"wwr" => 0.4,
                   "offset" => 1,
                   "application_type" => "Above Floor"})
-        
+
 #add ASHRAE System type 01, PTAC, Residential
 model.add_hvac({"ashrae_sys_num" => '01'})
 
 #add thermostats
 model.add_thermostats({"heating_setpoint" => 24,
                       "cooling_setpoint" => 28})
-              
+
 #assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions()
 
 #set whole building space type; simplified 90.1-2004 Large Office Whole Building
-model.set_space_type()  
+model.set_space_type()
 
 #add design days to the model (Chicago)
 model.add_design_days()
@@ -42,7 +42,7 @@ convectionCoefficients.setConvectionCoefficient1(1.0)
 convectionCoefficients.setConvectionCoefficient2Location("Outside")
 convectionCoefficients.setConvectionCoefficient2Type("Value")
 convectionCoefficients.setConvectionCoefficient2(1.0)
- 
+
 # set SurfacePropertyOtherSideCoefficients
 groundConditions = OpenStudio::Model::SurfacePropertyOtherSideCoefficients.new(model)
 groundConditions.setConstantTemperature(10.0)
@@ -81,4 +81,4 @@ OpenStudio::Model::OutputVariable.new("Surface Outside Face Convection Heat Tran
 #save the OpenStudio model (.osm)
 model.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
                            "osm_name" => "in.osm"})
-                           
+
