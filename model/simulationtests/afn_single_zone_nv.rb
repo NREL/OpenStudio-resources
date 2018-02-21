d2aaf8cf7dc2e6596197204689857b436dc77d11
Dan Macumber <dan.macumber@gmail.com>
2018-02-20 19:46:47 -0700

    Updating test to new API

--- model/simulationtests/afn_single_zone_nv.rb
+++ model/simulationtests/afn_single_zone_nv.rb
@@ -13,25 +13,25 @@ class SurfaceNetworkBuilder < SurfaceVisitor
   def interiorFloor(model, surface, adjacentSurface)
     if !surface.outsideBoundaryCondition().start_with?('Ground') then
       # Create a surface linkage
-      link = surface.createAirflowNetworkSurface(@interiorCrack)
+      link = surface.airflowNetworkSurface(@interiorCrack)
     end
   end
 
   def interiorRoofCeiling(model, surface, adjacentSurface)
     # Create a surface linkage
-    link = surface.createAirflowNetworkSurface.new(@interiorCrack)
+    link = surface.airflowNetworkSurface.new(@interiorCrack)
   end
 
   def interiorWall(model, surface, adjacentSurface)
     # Create a surface linkage
-    link = surface.createAirflowNetworkSurface(@interiorCrack)
+    link = surface.airflowNetworkSurface(@interiorCrack)
   end
 
   def exteriorSurface(model, surface)
     # Create an external node?
     if !surface.outsideBoundaryCondition().start_with?('Ground') then
       # Create a surface linkage
-      link = surface.createAirflowNetworkSurface(@exteriorCrack)
+      link = surface.airflowNetworkSurface(@exteriorCrack)
     end
   end
 end
@@ -82,8 +82,7 @@ afn_control.setAirflowNetworkControl("MultizoneWithoutDistribution")
 
 #make an afn zone
 zone = model.getThermalZones()[0] # There should only be one...
-optafnzone = zone.createAirflowNetworkZone
-afnzone = optafnzone.get
+afnzone = zone.airflowNetworkZone
 
 #connect up
 visitor = SurfaceNetworkBuilder.new(model)
