35d08607949c656cf4dd757fbcfbca5c4ac7ff97
Julien Marrec <julien.marrec@gmail.com>
2018-10-05 10:25:18 +0200

    Consistent saving method for the rest of the tests

--- model/simulationtests/lifecyclecostparameters.rb
+++ model/simulationtests/lifecyclecostparameters.rb
@@ -14,13 +14,12 @@ model.getConstructions.each do |construction|
       next
     end
   end
-  
+
   material_cost = OpenStudio::Model::LifeCycleCost::createLifeCycleCost("Material Cost", construction, 10.0, "CostPerArea", "Construction", 10, 0)
   demolition_cost = OpenStudio::Model::LifeCycleCost::createLifeCycleCost("Demolition Cost", construction, 2.0, "CostPerArea", "Replacement", 10, 10)
   salvage_cost = OpenStudio::Model::LifeCycleCost::createLifeCycleCost("Salvage Cost", construction, -1.0, "CostPerArea", "Replacement", 10, 10)
   maintenance_cost = OpenStudio::Model::LifeCycleCost::createLifeCycleCost("Maintenance Cost", construction, 1.0, "CostPerArea", "Maintenance", 1, 1)
 end
 
-model.save(OpenStudio::Path.new("in.osm") , true);
-
-
+# save the OpenStudio model (.osm)
+model.save_openstudio_osm({"osm_save_directory" => Dir.pwd, "osm_name" => "in.osm"})
