dbd8f50be6aa4dc9c8edb1ee0b6d3a9e97f0ce6e
Julien Marrec <julien.marrec@gmail.com>
2019-09-10 17:22:22 +0200

    Ok test appears fine. Also added the use of the new setter for Minimum Runtime Before Operating Mode Change

--- model/simulationtests/unitary_vav_bypass_plenum.rb
+++ model/simulationtests/unitary_vav_bypass_plenum.rb
@@ -6,20 +6,20 @@ m = BaselineModel.new
 
 #make a 2 story, 100m X 50m, 2 zone core/perimeter building
 m.add_geometry({"length" => 100,
-                    "width" => 50,
-                    "num_floors" => 2,
-                    "floor_to_floor_height" => 4,
-                    "plenum_height" => 1,
-                    "perimeter_zone_depth" => 0})
+                "width" => 50,
+                "num_floors" => 2,
+                "floor_to_floor_height" => 4,
+                "plenum_height" => 1,
+                "perimeter_zone_depth" => 0})
 
 #add windows at a 40% window-to-wall ratio
 m.add_windows({"wwr" => 0.4,
-                   "offset" => 1,
-                   "application_type" => "Above Floor"})
+               "offset" => 1,
+               "application_type" => "Above Floor"})
 
 #add thermostats
 m.add_thermostats({"heating_setpoint" => 24,
-                       "cooling_setpoint" => 28})
+                   "cooling_setpoint" => 28})
 
 #assign constructions from a local library to the walls/windows/etc. in the model
 m.set_constructions()
@@ -74,6 +74,8 @@ unitary.addToNode(a.supplyOutletNode)
 
 unitary.setPlenumorMixer(return_plenum)
 
+unitary.setMinimumRuntimeBeforeOperatingModeChange(0.25)
+
 #save the OpenStudio model (.osm)
-model.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
-                           "osm_name" => "in.osm"})
+m.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
+                       "osm_name" => "in.osm"})
