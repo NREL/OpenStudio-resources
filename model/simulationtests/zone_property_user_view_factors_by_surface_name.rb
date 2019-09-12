c83b74c61b8dfb7337d54a0a4ffef20f860d56d5
Julien Marrec <julien.marrec@gmail.com>
2019-09-12 16:30:31 +0200

    Make test smaller (5zones)

--- model/simulationtests/zone_property_user_view_factors_by_surface_name.rb
+++ model/simulationtests/zone_property_user_view_factors_by_surface_name.rb
@@ -3,14 +3,14 @@ require 'lib/baseline_model'
 
 model = BaselineModel.new
 
-#make a 2 story, 100m X 50m, 10 zone core/perimeter building
+#make a 1 story, 100m X 50m, 5 zone core/perimeter building
 model.add_geometry({"length" => 100,
                     "width" => 50,
-                    "num_floors" => 2,
+                    "num_floors" => 1,
                     "floor_to_floor_height" => 4,
                     "plenum_height" => 1,
                     "perimeter_zone_depth" => 3})
-                    
+
 #add windows at a 40% window-to-wall ratio
 model.add_windows({"wwr" => 0.4,
                    "offset" => 1,
@@ -22,7 +22,7 @@ model.add_hvac({"ashrae_sys_num" => '01'})
 #add thermostats
 model.add_thermostats({"heating_setpoint" => 24,
                        "cooling_setpoint" => 28})
-                       
+
 #assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions()
 
@@ -32,9 +32,12 @@ model.set_space_type()
 #add design days to the model (Chicago)
 model.add_design_days()
 
-# assign user view factors to all surfaces in a single thermal zone
+# In order to produce more consistent results between different runs,
+# we sort the zones by names
 thermal_zones = model.getThermalZones.sort_by{|z| z.name.to_s}
-thermal_zone = thermal_zones[1] # ensure a perimeter zone
+# assign user view factors to all surfaces in a single thermal zone, ensuring
+# this zone is a perimeter zone
+thermal_zone = thermal_zones[1]
 
 # create the zone property user view factors by surface name object
 zone_property_user_view_factors_by_surface_name = thermal_zone.getZonePropertyUserViewFactorsBySurfaceName
@@ -86,4 +89,5 @@ surfaces.each do |surface|
 end
 
 # save the OpenStudio model (.osm)
-model.save_openstudio_osm({"osm_save_directory" => Dir.pwd, "osm_name" => "in.osm"})
+model.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
+                           "osm_name" => "in.osm"})
