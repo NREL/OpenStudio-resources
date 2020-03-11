68e01e56398fd29cfa6f6f5866b1fddf9df28bcf
Julien Marrec <julien.marrec@gmail.com>
2020-03-11 17:28:25 +0100

    Add OSM output after runing the rb test with docker 2.0.5

--- model_tests.rb
+++ model_tests.rb
@@ -700,10 +700,9 @@ class ModelTests < Minitest::Test
     result = sim_test('sizing_zone_dszad.rb')
   end
 
-  # TODO:
-  #def test_sizing_zone_dszad_osm
-  #  result = sim_test('sizing_zone_dszad.osm')
-  #end
+  def test_sizing_zone_dszad_osm
+    result = sim_test('sizing_zone_dszad.osm')
+  end
 
   def test_solar_collector_flat_plate_water_rb
     result = sim_test('solar_collector_flat_plate_water.rb')
