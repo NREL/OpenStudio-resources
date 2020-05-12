ec5468a794fd6339ee170d83b4a74fc172bff5dd
Joe Robertson <joseph.robertson@nrel.gov>
2020-05-11 17:02:59 -0700

    Updates to the functional test.

--- model/simulationtests/surfacecontrol_moveableinsulation.rb
+++ model/simulationtests/surfacecontrol_moveableinsulation.rb
@@ -5,7 +5,7 @@ require 'lib/baseline_model'
 model = BaselineModel.new
 
 #make a 1 story, 100m X 50m, 1 zone core/perimeter building
-m.add_geometry({"length" => 100,
+model.add_geometry({"length" => 100,
                 "width" => 50,
                 "num_floors" => 1,
                 "floor_to_floor_height" => 4,
@@ -13,26 +13,26 @@ m.add_geometry({"length" => 100,
                 "perimeter_zone_depth" => 0})
 
 #add windows at a 40% window-to-wall ratio
-m.add_windows({"wwr" => 0.4,
+model.add_windows({"wwr" => 0.4,
                "offset" => 1,
                "application_type" => "Above Floor"})
 
 #add thermostats
-m.add_thermostats({"heating_setpoint" => 24,
+model.add_thermostats({"heating_setpoint" => 24,
                    "cooling_setpoint" => 28})
 
 #assign constructions from a local library to the walls/windows/etc. in the model
-m.set_constructions()
+model.set_constructions()
 
 #set whole building space type; simplified 90.1-2004 Large Office Whole Building
-m.set_space_type()
+model.set_space_type()
 
 #add design days to the model (Chicago)
-m.add_design_days()
+model.add_design_days()
 
 # In order to produce more consistent results between different runs,
 # we sort the zones by names (only one here anyways...)
-zones = m.getThermalZones.sort_by{|z| z.name.to_s}
+zones = model.getThermalZones.sort_by{|z| z.name.to_s}
 
 # Use Ideal Air Loads
 zones.each{|z| z.setUseIdealAirLoads(true)}
@@ -51,9 +51,8 @@ model.getSurfaces.each do |surface|
   next if surface.surfaceType != "Wall"
   next if surface.outsideBoundaryCondition != "Outdoors"
 
-  movableInsulation = OpenStudio::Model::SurfaceControlMovableInsulation.new(surface)
-  movableInsulation.setInsulationType("Outside")
-  movableInsulation.setMaterial(material)
+  movableInsulation = OpenStudio::Model::SurfaceControlMovableInsulation.new(surface, material)
+  movableInsulation.setInsulationType("Inside")
   movableInsulation.setSchedule(schedule)
 end
 
