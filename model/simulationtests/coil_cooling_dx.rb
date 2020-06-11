2c6c5f879b148ce50db19f2b8e420043c8516d9e
Joe Robertson <joseph.robertson@nrel.gov>
2020-06-11 13:58:34 -0700

    Continue writing test for coil cooling dx.

--- model/simulationtests/coil_cooling_dx.rb
+++ model/simulationtests/coil_cooling_dx.rb
@@ -7,7 +7,7 @@ m = BaselineModel.new
 #make a 2 story, 100m X 50m, 10 zone core/perimeter building
 m.add_geometry({"length" => 100,
                 "width" => 50,
-                "num_floors" => 2,
+                "num_floors" => 1,
                 "floor_to_floor_height" => 4,
                 "plenum_height" => 1,
                 "perimeter_zone_depth" => 3})
@@ -31,35 +31,168 @@ m.set_space_type()
 m.add_design_days()
 
 #add ASHRAE System type 07, VAV w/ Reheat
-m.add_hvac({"ashrae_sys_num" => '07'})
+# m.add_hvac({"ashrae_sys_num" => '07'})
 
 # In order to produce more consistent results between different runs,
 # we sort the zones by names
 zones = m.getThermalZones.sort_by{|z| z.name.to_s}
 
+def curve_biquadratic(model, c_1constant, c_2x, c_3xPOW2, c_4y, c_5yPOW2, c_6xTIMESY, minx, maxx, miny, maxy)
+  curve = OpenStudio::Model::CurveBiquadratic.new(model)
+  curve.setCoefficient1Constant(c_1constant)
+  curve.setCoefficient2x(c_2x)
+  curve.setCoefficient3xPOW2(c_3xPOW2)
+  curve.setCoefficient4y(c_4y)
+  curve.setCoefficient5yPOW2(c_5yPOW2)
+  curve.setCoefficient6xTIMESY(c_6xTIMESY)
+  curve.setMinimumValueofx(minx)
+  curve.setMaximumValueofx(maxx)
+  curve.setMinimumValueofy(miny)
+  curve.setMaximumValueofy(maxy)
+  return curve
+end
+
+def curve_quadratic(model, c_1constant, c_2x, c_3xPOW2, minx, maxx, miny, maxy)
+  curve = OpenStudio::Model::CurveQuadratic.new(model)
+  curve.setCoefficient1Constant(c_1constant)
+  curve.setCoefficient2x(c_2x)
+  curve.setCoefficient3xPOW2(c_3xPOW2)
+  curve.setMinimumValueofx(minx)
+  curve.setMaximumValueofx(maxx)
+  curve.setMinimumCurveOutput(miny)
+  curve.setMaximumCurveOutput(maxy)
+  return curve
+end
+
+constant_biquadratic = curve_biquadratic(m, 1, 0, 0, 0, 0, 0, -100, 100, -100, 100)
+
 # CoilCoolingDXCurveFitSpeed
-curve_fit_speed_1 = OpenStudio::Model::CoilCoolingDXCurveFitSpeed.new(m)
+speed_1 = OpenStudio::Model::CoilCoolingDXCurveFitSpeed.new(m)
+speed_1.setGrossTotalCoolingCapacityFraction(4015.05615933448 / 4015.05615933448)
+speed_1.setEvaporatorAirFlowRateFraction(0.241052070735995 / 0.241052070735995)
+# speed_1.setCondenserAirFlowRateFraction()
+speed_1.setGrossSensibleHeatRatio(0.842150793933333)
+speed_1.setGrossCoolingCOP(5.48021287249984)
+
+cool_cap_ft = curve_biquadratic(m, 1.790226088881, -0.0772146982404, 0.00299548780452, 0.0026270330994, -6.81238188e-005, -0.00062105857056, 13.88, 23.88, 18.33, 51.66)
+cool_cap_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_eir_ft = curve_biquadratic(m, -0.1450569952, 0.062239559472, -0.00190953288, -0.012608055432, 0.0010591834752, -0.0003311985672, 13.88, 23.88, 18.33, 51.66)
+cool_eir_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_plf_fplr = curve_quadratic(m, 0.75, 0.25, 0, 0, 1, 0.7, 1)
+
+speed_1.setTotalCoolingCapacityModifierFunctionofTemperatureCurve(cool_cap_ft)
+speed_1.setTotalCoolingCapacityModifierFunctionofAirFlowFractionCurve(cool_cap_fff)
+speed_1.setEnergyInputRatioModifierFunctionofTemperatureCurve(cool_eir_ft)
+speed_1.setEnergyInputRatioModifierFunctionofAirFlowFractionCurve(cool_eir_fff)
+speed_1.setPartLoadFractionCorrelationCurve(cool_plf_fplr)
+speed_1.setWasteHeatModifierFunctionofTemperatureCurve(constant_biquadratic)
+
+speed_2 = OpenStudio::Model::CoilCoolingDXCurveFitSpeed.new(m)
+speed_2.setGrossTotalCoolingCapacityFraction(7137.87761659463 / 4015.05615933448)
+speed_2.setEvaporatorAirFlowRateFraction(0.397026940035757 / 0.241052070735995)
+# speed_2.setCondenserAirFlowRateFraction()
+speed_2.setGrossSensibleHeatRatio(0.80758872085)
+speed_2.setGrossCoolingCOP(5.1989191865934)
+
+cool_cap_ft = curve_biquadratic(m, 1.189356223401, -0.0251898606522, 0.0018013099626, 0.004151872233, -4.332993588e-005, -0.0006851085138, 13.88, 23.88, 18.33, 51.66)
+cool_cap_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_eir_ft = curve_biquadratic(m, 1.21042990764, -0.076844452176, 0.00151658244, -0.002526115752, 0.0007214803488, -0.0001603816524, 13.88, 23.88, 18.33, 51.66)
+cool_eir_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_plf_fplr = curve_quadratic(m, 0.75, 0.25, 0, 0, 1, 0.7, 1)
+
+speed_2.setTotalCoolingCapacityModifierFunctionofTemperatureCurve(cool_cap_ft)
+speed_2.setTotalCoolingCapacityModifierFunctionofAirFlowFractionCurve(cool_cap_fff)
+speed_2.setEnergyInputRatioModifierFunctionofTemperatureCurve(cool_eir_ft)
+speed_2.setEnergyInputRatioModifierFunctionofAirFlowFractionCurve(cool_eir_fff)
+speed_2.setPartLoadFractionCorrelationCurve(cool_plf_fplr)
+speed_2.setWasteHeatModifierFunctionofTemperatureCurve(constant_biquadratic)
+
+speed_3 = OpenStudio::Model::CoilCoolingDXCurveFitSpeed.new(m)
+speed_3.setGrossTotalCoolingCapacityFraction(11152.9337759291 / 4015.05615933448)
+speed_3.setEvaporatorAirFlowRateFraction(0.472651119090187 / 0.241052070735995)
+# speed_3.setCondenserAirFlowRateFraction()
+speed_3.setGrossSensibleHeatRatio(0.7039025016)
+speed_3.setGrossCoolingCOP(4.64414572911775)
+
+cool_cap_ft = curve_biquadratic(m, -0.300216325722, 0.1194562230534, -0.001862843184, 0.000730227277800001, -3.753475524e-005, -0.00043911348696, 13.88, 23.88, 18.33, 51.66)
+cool_cap_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_eir_ft = curve_biquadratic(m, 0.70183064548, -0.049235917464, 0.00126482472, 0.031408426368, 0.0003622673484, -0.0011050729236, 13.88, 23.88, 18.33, 51.66)
+cool_eir_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_plf_fplr = curve_quadratic(m, 0.75, 0.25, 0, 0, 1, 0.7, 1)
+
+speed_3.setTotalCoolingCapacityModifierFunctionofTemperatureCurve(cool_cap_ft)
+speed_3.setTotalCoolingCapacityModifierFunctionofAirFlowFractionCurve(cool_cap_fff)
+speed_3.setEnergyInputRatioModifierFunctionofTemperatureCurve(cool_eir_ft)
+speed_3.setEnergyInputRatioModifierFunctionofAirFlowFractionCurve(cool_eir_fff)
+speed_3.setPartLoadFractionCorrelationCurve(cool_plf_fplr)
+speed_3.setWasteHeatModifierFunctionofTemperatureCurve(constant_biquadratic)
+
+speed_4 = OpenStudio::Model::CoilCoolingDXCurveFitSpeed.new(m)
+speed_4.setGrossTotalCoolingCapacityFraction(12937.4031800778 / 4015.05615933448)
+speed_4.setEvaporatorAirFlowRateFraction(0.562454831717322 / 0.241052070735995)
+# speed_4.setCondenserAirFlowRateFraction()
+speed_4.setGrossSensibleHeatRatio(0.712483430089655)
+speed_4.setGrossCoolingCOP(4.0695894950265)
+
+cool_cap_ft = curve_biquadratic(m, 0.962249880784, -0.0039087090378, 0.00132934689648, 0.0016072565382, -8.342352e-008, -0.00065486142576, 13.88, 23.88, 18.33, 51.66)
+cool_cap_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_eir_ft = curve_biquadratic(m, -0.74798782608, 0.099914996016, -0.00272789532, 0.026966547, 0.0002513297808, -0.0006019728516, 13.88, 23.88, 18.33, 51.66)
+cool_eir_fff = curve_quadratic(m, 1, 0, 0, 0, 2, 0, 2)
+cool_plf_fplr = curve_quadratic(m, 0.75, 0.25, 0, 0, 1, 0.7, 1)
+
+speed_4.setTotalCoolingCapacityModifierFunctionofTemperatureCurve(cool_cap_ft)
+speed_4.setTotalCoolingCapacityModifierFunctionofAirFlowFractionCurve(cool_cap_fff)
+speed_4.setEnergyInputRatioModifierFunctionofTemperatureCurve(cool_eir_ft)
+speed_4.setEnergyInputRatioModifierFunctionofAirFlowFractionCurve(cool_eir_fff)
+speed_4.setPartLoadFractionCorrelationCurve(cool_plf_fplr)
+speed_4.setWasteHeatModifierFunctionofTemperatureCurve(constant_biquadratic)
 
 # CoilCoolingDXCurveFitOperatingMode
-curve_fit_operating_mode = OpenStudio::Model::CoilCoolingDXCurveFitOperatingMode.new(m)
-curve_fit_operating_mode.addSpeed(curve_fit_speed_1)
+operating_mode = OpenStudio::Model::CoilCoolingDXCurveFitOperatingMode.new(m)
+operating_mode.setRatedGrossTotalCoolingCapacity(4015.05615933448)
+operating_mode.setRatedEvaporatorAirFlowRate(0.241052070735995)
+# operating_mode.setRatedCondenserAirFlowRate()
+operating_mode.setMaximumCyclingRate(3)
+operating_mode.setRatioofInitialMoistureEvaporationRateandSteadyStateLatentCapacity(1.5)
+operating_mode.setLatentCapacityTimeConstant(45)
+operating_mode.setNominalTimeforCondensateRemovaltoBegin(1000)
+
+operating_mode.addSpeed(speed_1)
+operating_mode.addSpeed(speed_2)
+operating_mode.addSpeed(speed_3)
+operating_mode.addSpeed(speed_4)
+operating_mode.setNominalSpeedNumber(1)
 
 # CoilCoolingDXCurveFitPerformance
-curve_fit_performance = OpenStudio::Model::CoilCoolingDXCurveFitPerformance.new(m, curve_fit_operating_mode)
+performance = OpenStudio::Model::CoilCoolingDXCurveFitPerformance.new(m, operating_mode)
 
 # CoilCoolingDX
-coil = OpenStudio::Model::CoilCoolingDX.new(m, curve_fit_performance)
+coil = OpenStudio::Model::CoilCoolingDX.new(m, performance)
+
+# FanOnOff
+fan = OpenStudio::Model::FanOnOff.new(m, m.alwaysOnDiscreteSchedule)
+fan.setFanEfficiency(0.75)
+fan.setPressureRise(476.748000740096)
+fan.setMotorEfficiency(1.0)
+fan.setMotorInAirstreamFraction(1.0)
 
 # AirLoopHVACUnitarySystem
 air_loop_unitary = OpenStudio::Model::AirLoopHVACUnitarySystem.new(m)
+air_loop_unitary.setAvailabilitySchedule(m.alwaysOnDiscreteSchedule)
+air_loop_unitary.setCoolingCoil(coil)
+air_loop_unitary.setSupplyAirFlowRateDuringHeatingOperation(0.0)
+air_loop_unitary.setSupplyFan(fan)
+air_loop_unitary.setFanPlacement("BlowThrough")
+air_loop_unitary.setSupplyAirFanOperatingModeSchedule(m.alwaysOffDiscreteSchedule)
+air_loop_unitary.setMaximumSupplyAirTemperature(48.888889)
+air_loop_unitary.setSupplyAirFlowRateWhenNoCoolingorHeatingisRequired(0)
+air_loop_unitary.setControllingZoneorThermostatLocation(zones[0])
 
 # AirLoopHVAC
 air_loop = OpenStudio::Model::AirLoopHVAC.new(m)
 air_supply_inlet_node = air_loop.supplyInletNode
-
 air_loop_unitary.addToNode(air_supply_inlet_node)
-air_loop_unitary.setCoolingCoil(coil)
-air_loop_unitary.setControllingZoneorThermostatLocation(zones[0])
+air_loop.multiAddBranchForZone(zones[0])
 
 #save the OpenStudio model (.osm)
 m.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
