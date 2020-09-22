c8f484feb4838a781ebad64d3d18c447feb16d26
Joe Robertson <joseph.robertson@nrel.gov>
2020-09-22 11:29:34 -0700

    Update complexity of the test.

--- model/simulationtests/shadingcontrol.rb
+++ model/simulationtests/shadingcontrol.rb
@@ -36,27 +36,49 @@ zones = m.getThermalZones.sort_by{|z| z.name.to_s}
 # Use Ideal Air Loads
 zones.each{|z| z.setUseIdealAirLoads(true)}
 
-# shading material
-blind = OpenStudio::Model::Blind.new(m)
+# shading material 1
+blind1 = OpenStudio::Model::Blind.new(m)
+
+# shading material 2
+blind2 = OpenStudio::Model::Blind.new(m)
 
 # shading control 1
-shading_control1 = OpenStudio::Model::ShadingControl.new(blind)
+shading_control1 = OpenStudio::Model::ShadingControl.new(blind1)
 
 # shading control 2
-shading_control2 = OpenStudio::Model::ShadingControl.new(blind)
+shading_control2 = OpenStudio::Model::ShadingControl.new(blind1)
+
+# shading control 3
+shading_control3 = OpenStudio::Model::ShadingControl.new(blind2)
+shading_control3.setTypeofSlatAngleControlforBlinds("BlockBeamSolar")
 
 # sub surfaces
 sub_surfaces = m.getSubSurfaces.sort_by{|ss| ss.name.to_s}
 sub_surface1 = sub_surfaces[0]
 sub_surface2 = sub_surfaces[1]
-# shading_control1.addSubSurfaces([sub_surface1, sub_surface2])
-# shading_control2.addSubSurfaces([sub_surface1, sub_surface2])
-# sub_surface1.addShadingControls([shading_control1, shading_control2])
-# sub_surface2.addShadingControls([shading_control1, shading_control2])
-sub_surface1.addShadingControl(shading_control1)
-sub_surface1.addShadingControl(shading_control2)
-sub_surface2.addShadingControl(shading_control1)
-sub_surface2.addShadingControl(shading_control2)
+sub_surface3 = sub_surfaces[2]
+sub_surface4 = sub_surfaces[3]
+
+# add sub surfaces to shading control 1
+shading_control1.addSubSurface(sub_surface1)
+
+# bulk add sub surfaces to shading control 2
+sub_surfaces = OpenStudio::Model::SubSurfaceVector.new
+[sub_surface2].each do |sub_surface|
+  sub_surfaces << sub_surface
+end
+shading_control2.addSubSurfaces(sub_surfaces)
+
+# add shading controls to sub suface 3
+sub_surface3.addShadingControl(shading_control1)
+sub_surface3.addShadingControl(shading_control2)
+
+# bulk add shading controls to sub surface 4
+shading_controls = OpenStudio::Model::ShadingControlVector.new
+[shading_control3].each do |shading_control|
+  shading_controls << shading_control
+end
+sub_surface4.addShadingControls(shading_controls)
 
 #save the OpenStudio model (.osm)
 m.save_openstudio_osm({"osm_save_directory" => Dir.pwd,
