74e996dcb2830473608905daa0ae7732c0dec37d
Julien Marrec <julien.marrec@gmail.com>
2020-09-23 16:28:53 +0200

    Update high level tests to check for missing OSMs while handling the TODO comments in there. Add it to PR workflow

--- highlevel_tests.rb
+++ highlevel_tests.rb
@@ -60,6 +60,12 @@ class HighLevelTests < MiniTest::Test
                           # Not enabled
                           'afn_single_zone_ac.rb']
 
+    content = File.read('model_tests.rb')
+    sim_test_commented_out_re = /TODO[ :\w ]+(\d\.\d\.\d).*?#\s*result = sim_test\('([\w\.]+)'\)/m
+    # eg: [["3.1.0", "coil_cooling_dx.osm"], ["3.1.0", "swimmingpool_indoor.osm"]]
+    matches = content.scan(sim_test_commented_out_re)
+    files_with_todo =  matches.map{|m| m[1]}
+
     base_dir = $ModelDir
     all_ruby_paths = Dir.glob(File.join(base_dir, '*.rb'));
     all_ruby_filenames = all_ruby_paths.map{|p| File.basename(p)};
@@ -67,7 +73,8 @@ class HighLevelTests < MiniTest::Test
     all_ruby_filenames.each do |filename|
       if !noMatchingOSMTests.include?(filename)
         # Check if there is a matching OSM file
-        matching_osm = File.join(base_dir, filename.sub('.rb', '.osm'))
+        osm_name = filename.sub('.rb', '.osm')
+        matching_osm = File.join(base_dir, osm_name)
 
         # If you want to be stricter than warn, uncomment this
         # assert File.exists?(matching_osm), "There is no matching OSM test for #{filename}"
@@ -87,10 +94,37 @@ class HighLevelTests < MiniTest::Test
             skip "Matching OSM Model version is newer than the SDK version used (#{model_version} versus #{$SdkVersion})"
           end
         else
-          # If there isn't a matching, we warn, but we'll still run it
-          # It might make sense if you have just added it recently
-          warn "There is no matching OSM test for #{filename}"
+          # If there isn't a match, let's just if there's a TODO in
+          # model_tests.rb
+          if !files_with_todo.include?(osm_name)
+            msg = "There is no matching OSM test for #{filename}. If you recently added the test please add this in model_tests.rb\n"
+            msg += "```\n"
+            msg += "  # TODO: To be added in the next official release after: #{OpenStudio::openStudioVersion}\n"
+            msg += "  def test_#{osm_name.sub('.osm', '_osm')}\n"
+            msg += "    result = sim_test('#{osm_name.sub('.osm', '_osm')}')\n"
+            msg += "  end\n"
+            msg += "```"
+            fail msg
+          end
         end
+      end
+    end
+  end
+
+  # Ensures that any pending OSMs are added once the official version is out
+  def test_ensure_pending_osms_are_added
+    # Don't deal with pre-release
+    if !OpenStudio::openStudioVersionPrerelease.empty?
+      skip  "Current version is a prerelease"
+    end
+
+    content = File.read('model_tests.rb');
+    sim_test_commented_out_re = /TODO[ :\w ]+(\d\.\d\.\d).*?#\s*result = sim_test\('([\w\.]+)'\)/m
+    # eg: [["3.1.0", "coil_cooling_dx.osm"], ["3.1.0", "swimmingpool_indoor.osm"]]
+    matches = content.scan(sim_test_commented_out_re);
+    matches.each do |v, t|
+      if Gem::Version.new($SdkVersion) > Gem::Version.new(v)
+        fail "#{t} was expected to be added after #{v}, we're already at #{$SdkVersion}"
       end
     end
   end
@@ -134,5 +168,3 @@ class HighLevelTests < MiniTest::Test
   end
 
 end
-
-
