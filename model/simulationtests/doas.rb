fce26be3fa4dcf95fff7f25da7a5513d15dc4e02
Joe Robertson <joseph.robertson@nrel.gov>
2020-11-11 13:53:46 -0700

    Add doas test.

---
+++ model/simulationtests/doas.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'openstudio'
+require_relative 'lib/baseline_model'
+
+model = BaselineModel.new
+
+# make a 2 story, 100m X 50m, 10 zone core/perimeter building
+model.add_geometry({ 'length' => 100,
+                     'width' => 50,
+                     'num_floors' => 1,
+                     'floor_to_floor_height' => 4,
+                     'plenum_height' => 1,
+                     'perimeter_zone_depth' => 0 })
+
+# add windows at a 40% window-to-wall ratio
+model.add_windows({ 'wwr' => 0.4,
+                    'offset' => 1,
+                    'application_type' => 'Above Floor' })
+
+# In order to produce more consistent results between different runs,
+# we sort the zones by names
+zones = model.getThermalZones.sort_by { |z| z.name.to_s }
+zone = zones[0]
+
+controller = OpenStudio::Model::ControllerOutdoorAir.new(model)
+oas = OpenStudio::Model::AirLoopHVACOutdoorAirSystem.new(model, controller)
+doas = OpenStudio::Model::AirLoopHVACDedicatedOutdoorAirSystem.new(model, oas)
+airloop = OpenStudio::Model::AirLoopHVAC.new(model)
+
+oas.addToNode(airloop.supplyOutletNode)
+doas.addAirLoop(airloop)
+airloop.addBranchForZone(zone)
+
+# add thermostats
+model.add_thermostats({ 'heating_setpoint' => 24,
+                        'cooling_setpoint' => 28 })
+
+# assign constructions from a local library to the walls/windows/etc. in the model
+model.set_constructions
+
+# set whole building space type; simplified 90.1-2004 Large Office Whole Building
+model.set_space_type
+
+# add design days to the model (Chicago)
+model.add_design_days
+
+# save the OpenStudio model (.osm)
+model.save_openstudio_osm({ 'osm_save_directory' => Dir.pwd, 'osm_name' => 'in.osm' })
