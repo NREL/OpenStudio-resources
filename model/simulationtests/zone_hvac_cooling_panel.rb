a33bedfa12257e355594510118a3af6033ecb225
Joe Robertson <joseph.robertson@nrel.gov>
2020-12-17 09:15:41 -0700

    Update test and get passing.

--- model/simulationtests/zone_hvac_cooling_panel.rb
+++ model/simulationtests/zone_hvac_cooling_panel.rb
@@ -5,7 +5,7 @@ require 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# make a 3 story, 100m X 50m, 10 zone core/perimeter building
+# make a 1 story, 100m X 50m, 10 zone core/perimeter building
 model.add_geometry({ 'length' => 100,
                      'width' => 50,
                      'num_floors' => 1,
@@ -22,49 +22,54 @@ model.add_windows({ 'wwr' => 0.4,
 model.add_thermostats({ 'heating_setpoint' => 24,
                         'cooling_setpoint' => 28 })
 
-# chilled Water Temp Schedule
-osTime = OpenStudio::Time.new(0, 24, 0, 0)
-# Schedule Ruleset
-chilled_water_temp_sch = OpenStudio::Model::ScheduleRuleset.new(model)
-chilled_water_temp_sch.setName('Chilled_Water_Temperature')
-# Winter Design Day
-chilled_water_temp_schWinter = OpenStudio::Model::ScheduleDay.new(model)
-chilled_water_temp_sch.setWinterDesignDaySchedule(chilled_water_temp_schWinter)
-chilled_water_temp_sch.winterDesignDaySchedule.setName('Chilled_Water_Temperature_Winter_Design_Day')
-chilled_water_temp_sch.winterDesignDaySchedule.addValue(osTime, 6.7)
-# Summer Design Day
-chilled_water_temp_schSummer = OpenStudio::Model::ScheduleDay.new(model)
-chilled_water_temp_sch.setSummerDesignDaySchedule(chilled_water_temp_schSummer)
-chilled_water_temp_sch.summerDesignDaySchedule.setName('Chilled_Water_Temperature_Summer_Design_Day')
-chilled_water_temp_sch.summerDesignDaySchedule.addValue(osTime, 6.7)
-# All other days
-chilled_water_temp_sch.defaultDaySchedule.setName('Chilled_Water_Temperature_Default')
-chilled_water_temp_sch.defaultDaySchedule.addValue(osTime, 6.7)
-
 # Chilled Water Plant
-chilledWaterPlant = OpenStudio::Model::PlantLoop.new(model)
-chilledWaterPlant.setName('Chilled Water Plant')
-chilledWaterSizing = chilledWaterPlant.sizingPlant
-chilledWaterSizing.setLoopType('Cooling')
-chilledWaterSizing.setDesignLoopExitTemperature(7.22)
-chilledWaterSizing.setLoopDesignTemperatureDifference(6.67)
-chilledWaterOutletNode = chilledWaterPlant.supplyOutletNode
-chilledWaterInletNode = chilledWaterPlant.supplyInletNode
-chilledWaterDemandOutletNode = chilledWaterPlant.demandOutletNode
-chilledWaterDemandInletNode = chilledWaterPlant.demandInletNode
-chilledWaterSPM = OpenStudio::Model::SetpointManagerScheduled.new(model, chilled_water_temp_sch)
-chilledWaterSPM.addToNode(chilledWaterOutletNode)
+chw_loop = OpenStudio::Model::PlantLoop.new(model)
+chw_loop.setName('Chilled Water Loop')
+chw_loop.setMaximumLoopTemperature(98)
+chw_loop.setMinimumLoopTemperature(1)
+chw_temp_f = 44
+chw_delta_t_r = 10.1 # 10.1F delta-T
+chw_temp_c = OpenStudio.convert(chw_temp_f, 'F', 'C').get
+chw_delta_t_k = OpenStudio.convert(chw_delta_t_r, 'R', 'K').get
+chw_temp_sch = OpenStudio::Model::ScheduleRuleset.new(model)
+chw_temp_sch.defaultDaySchedule.addValue(OpenStudio::Time.new(0, 24, 0, 0), chw_temp_c)
+chw_stpt_manager = OpenStudio::Model::SetpointManagerScheduled.new(model, chw_temp_sch)
+chw_stpt_manager.addToNode(chw_loop.supplyOutletNode)
+sizing_plant = chw_loop.sizingPlant
+sizing_plant.setLoopType('Cooling')
+sizing_plant.setDesignLoopExitTemperature(chw_temp_c)
+sizing_plant.setLoopDesignTemperatureDifference(chw_delta_t_k)
 
-# pump
-chilledWaterPump = OpenStudio::Model::PumpVariableSpeed.new(model)
-chilledWaterPump.addToNode(chilledWaterInletNode)
+# Primary chilled water pump
+pri_chw_pump = OpenStudio::Model::HeaderedPumpsConstantSpeed.new(model)
+pri_chw_pump.setName('Chilled Water Loop Primary Pump')
+pri_chw_pump_head_ft_h2o = 15
+pri_chw_pump_head_press_pa = OpenStudio.convert(pri_chw_pump_head_ft_h2o, 'ftH_{2}O', 'Pa').get
+pri_chw_pump.setRatedPumpHead(pri_chw_pump_head_press_pa)
+pri_chw_pump.setMotorEfficiency(0.9)
+pri_chw_pump.setPumpControlType('Intermittent')
+pri_chw_pump.addToNode(chw_loop.supplyInletNode)
+# Secondary chilled water pump
+sec_chw_pump = OpenStudio::Model::HeaderedPumpsVariableSpeed.new(model)
+sec_chw_pump.setName('Chilled Water Loop Secondary Pump')
+sec_chw_pump_head_ft_h2o = 45
+sec_chw_pump_head_press_pa = OpenStudio.convert(sec_chw_pump_head_ft_h2o, 'ftH_{2}O', 'Pa').get
+sec_chw_pump.setRatedPumpHead(sec_chw_pump_head_press_pa)
+sec_chw_pump.setMotorEfficiency(0.9)
+# Curve makes it perform like variable speed pump
+sec_chw_pump.setFractionofMotorInefficienciestoFluidStream(0)
+sec_chw_pump.setCoefficient1ofthePartLoadPerformanceCurve(0)
+sec_chw_pump.setCoefficient2ofthePartLoadPerformanceCurve(0.0205)
+sec_chw_pump.setCoefficient3ofthePartLoadPerformanceCurve(0.4101)
+sec_chw_pump.setCoefficient4ofthePartLoadPerformanceCurve(0.5753)
+sec_chw_pump.setPumpControlType('Intermittent')
+sec_chw_pump.addToNode(chw_loop.demandInletNode)
+# Change the chilled water loop to have a two-way common pipes
+chw_loop.setCommonPipeSimulation('CommonPipe')
 
-# district cooling
-district_cooling = OpenStudio::Model::DistrictCooling.new(model)
-chilledWaterPlant.addSupplyBranchForComponent(district_cooling)
-
-chilledWaterDemandBypass = OpenStudio::Model::PipeAdiabatic.new(model)
-chilledWaterPlant.addSupplyBranchForComponent(chilledWaterDemandBypass)
+# Cooling equipment
+chiller = OpenStudio::Model::ChillerElectricEIR.new(model)
+chw_loop.addSupplyBranchForComponent(chiller)
 
 # assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions
@@ -80,8 +85,9 @@ story_1_core_thermal_zone = model.getThermalZoneByName('Story 1 Core Thermal Zon
 
 # Add ZoneHVACCoolingPanelRadiantConvectiveWater
 zoneHVACCoolingPanelRadiantConvectiveWater = OpenStudio::Model::ZoneHVACCoolingPanelRadiantConvectiveWater.new(model)
-panel_coil = zoneHVACCoolingPanelRadiantConvectiveWater.coolingCoil
-chilledWaterPlant.addDemandBranchForComponent(panel_coil)
+panel_coil = zoneHVACCoolingPanelRadiantConvectiveWater.coolingCoil.to_CoilCoolingWaterPanelRadiant.get
+panel_coil.setCoolingDesignCapacity(600.0)
+chw_loop.addDemandBranchForComponent(panel_coil)
 zoneHVACCoolingPanelRadiantConvectiveWater.addToThermalZone(story_1_core_thermal_zone)
 
 # save the OpenStudio model (.osm)
