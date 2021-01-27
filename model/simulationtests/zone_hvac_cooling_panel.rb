a7c44f77df759f10421c64ca8f07d89606eeb134
Julien Marrec <julien.marrec@gmail.com>
2021-01-27 15:32:47 +0100

    Make review changes

--- model/simulationtests/zone_hvac_cooling_panel.rb
+++ model/simulationtests/zone_hvac_cooling_panel.rb
@@ -5,22 +5,41 @@ require 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# make a 1 story, 100m X 50m, 10 zone core/perimeter building
+# make a 1 story, 100m X 50m, 1 zone building
 model.add_geometry({ 'length' => 100,
                      'width' => 50,
                      'num_floors' => 1,
                      'floor_to_floor_height' => 4,
-                     'plenum_height' => 1,
-                     'perimeter_zone_depth' => 3 })
+                     'plenum_height' => 0,
+                     'perimeter_zone_depth' => 0 })
 
 # add windows at a 40% window-to-wall ratio
 model.add_windows({ 'wwr' => 0.4,
                     'offset' => 1,
                     'application_type' => 'Above Floor' })
 
+
+# assign constructions from a local library to the walls/windows/etc. in the model
+model.set_constructions
+
+# set whole building space type; simplified 90.1-2004 Large Office Whole Building
+model.set_space_type
+
+# add design days to the model (Chicago)
+model.add_design_days
+
 # add thermostats
 model.add_thermostats({ 'heating_setpoint' => 24,
                         'cooling_setpoint' => 28 })
+
+
+# In order to produce more consistent results between different runs,
+# we sort the zones by names
+# (There's only one here, but just in case this would be copy pasted somewhere
+# else...)
+zones = model.getThermalZones.sort_by { |z| z.name.to_s }
+z = zones[0]
+
 
 # Chilled Water Plant
 chw_loop = OpenStudio::Model::PlantLoop.new(model)
@@ -49,6 +68,7 @@ pri_chw_pump.setRatedPumpHead(pri_chw_pump_head_press_pa)
 pri_chw_pump.setMotorEfficiency(0.9)
 pri_chw_pump.setPumpControlType('Intermittent')
 pri_chw_pump.addToNode(chw_loop.supplyInletNode)
+
 # Secondary chilled water pump
 sec_chw_pump = OpenStudio::Model::HeaderedPumpsVariableSpeed.new(model)
 sec_chw_pump.setName('Chilled Water Loop Secondary Pump')
@@ -56,6 +76,7 @@ sec_chw_pump_head_ft_h2o = 45
 sec_chw_pump_head_press_pa = OpenStudio.convert(sec_chw_pump_head_ft_h2o, 'ftH_{2}O', 'Pa').get
 sec_chw_pump.setRatedPumpHead(sec_chw_pump_head_press_pa)
 sec_chw_pump.setMotorEfficiency(0.9)
+
 # Curve makes it perform like variable speed pump
 sec_chw_pump.setFractionofMotorInefficienciestoFluidStream(0)
 sec_chw_pump.setCoefficient1ofthePartLoadPerformanceCurve(0)
@@ -64,6 +85,7 @@ sec_chw_pump.setCoefficient3ofthePartLoadPerformanceCurve(0.4101)
 sec_chw_pump.setCoefficient4ofthePartLoadPerformanceCurve(0.5753)
 sec_chw_pump.setPumpControlType('Intermittent')
 sec_chw_pump.addToNode(chw_loop.demandInletNode)
+
 # Change the chilled water loop to have a two-way common pipes
 chw_loop.setCommonPipeSimulation('CommonPipe')
 
@@ -71,24 +93,12 @@ chw_loop.setCommonPipeSimulation('CommonPipe')
 chiller = OpenStudio::Model::ChillerElectricEIR.new(model)
 chw_loop.addSupplyBranchForComponent(chiller)
 
-# assign constructions from a local library to the walls/windows/etc. in the model
-model.set_constructions
-
-# set whole building space type; simplified 90.1-2004 Large Office Whole Building
-model.set_space_type
-
-# add design days to the model (Chicago)
-model.add_design_days
-
-# assign thermal zones to variables
-story_1_core_thermal_zone = model.getThermalZoneByName('Story 1 Core Thermal Zone').get
-
 # Add ZoneHVACCoolingPanelRadiantConvectiveWater
 zoneHVACCoolingPanelRadiantConvectiveWater = OpenStudio::Model::ZoneHVACCoolingPanelRadiantConvectiveWater.new(model)
 panel_coil = zoneHVACCoolingPanelRadiantConvectiveWater.coolingCoil.to_CoilCoolingWaterPanelRadiant.get
 panel_coil.setCoolingDesignCapacity(600.0)
 chw_loop.addDemandBranchForComponent(panel_coil)
-zoneHVACCoolingPanelRadiantConvectiveWater.addToThermalZone(story_1_core_thermal_zone)
+zoneHVACCoolingPanelRadiantConvectiveWater.addToThermalZone(z)
 
 # save the OpenStudio model (.osm)
 model.save_openstudio_osm({ 'osm_save_directory' => Dir.pwd,
