94db91d5c4ce82727d5ddb79721b4551157f06e6
Julien Marrec <julien.marrec@gmail.com>
2021-01-27 15:44:44 +0100

    Add TODO and adjust test slightly

--- model/simulationtests/doas.rb
+++ model/simulationtests/doas.rb
@@ -5,33 +5,18 @@ require_relative 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# make a 2 story, 100m X 50m, 10 zone core/perimeter building
+# make a 1 story, 100m X 50m, 1 zone building
 model.add_geometry({ 'length' => 100,
                      'width' => 50,
                      'num_floors' => 1,
                      'floor_to_floor_height' => 4,
-                     'plenum_height' => 1,
+                     'plenum_height' => 0,
                      'perimeter_zone_depth' => 0 })
 
 # add windows at a 40% window-to-wall ratio
 model.add_windows({ 'wwr' => 0.4,
                     'offset' => 1,
                     'application_type' => 'Above Floor' })
-
-# In order to produce more consistent results between different runs,
-# we sort the zones by names
-zones = model.getThermalZones.sort_by { |z| z.name.to_s }
-zone = zones[0]
-
-controller = OpenStudio::Model::ControllerOutdoorAir.new(model)
-oas = OpenStudio::Model::AirLoopHVACOutdoorAirSystem.new(model, controller)
-doas = OpenStudio::Model::AirLoopHVACDedicatedOutdoorAirSystem.new(model, oas)
-airloop = OpenStudio::Model::AirLoopHVAC.new(model)
-
-oas.addToNode(airloop.supplyOutletNode)
-doas.addAirLoop(airloop)
-airloop.addBranchForZone(zone)
-
 # add thermostats
 model.add_thermostats({ 'heating_setpoint' => 24,
                         'cooling_setpoint' => 28 })
@@ -44,6 +29,20 @@ model.set_space_type
 
 # add design days to the model (Chicago)
 model.add_design_days
+
+
+# In order to produce more consistent results between different runs,
+# we sort the zones by names
+zones = model.getThermalZones.sort_by { |z| z.name.to_s }
+
+controller = OpenStudio::Model::ControllerOutdoorAir.new(model)
+oas = OpenStudio::Model::AirLoopHVACOutdoorAirSystem.new(model, controller)
+doas = OpenStudio::Model::AirLoopHVACDedicatedOutdoorAirSystem.new(model, oas)
+airloop = OpenStudio::Model::AirLoopHVAC.new(model)
+
+oas.addToNode(airloop.supplyOutletNode)
+doas.addAirLoop(airloop)
+airloop.addBranchForZone(zone)
 
 # save the OpenStudio model (.osm)
 model.save_openstudio_osm({ 'osm_save_directory' => Dir.pwd, 'osm_name' => 'in.osm' })
