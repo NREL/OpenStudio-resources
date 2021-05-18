99b2427e62e6a9f3743fbe52f6df2dcdeb812ced
Julien Marrec <julien.marrec@gmail.com>
2021-05-18 10:26:24 +0200

    Add pending OSMs with 3.2.0 official for #119

--- model_tests.rb
+++ model_tests.rb
@@ -81,19 +81,17 @@ class ModelTests < Minitest::Test
     result = sim_test('ptac_othercoils.rb')
   end
 
-  # TODO: To be added in the next official release after: 3.1.0
-  # def test_ptac_othercoils_osm
-  # result = sim_test('ptac_othercoils.osm')
-  # end
+  def test_ptac_othercoils_osm
+    result = sim_test('ptac_othercoils.osm')
+  end
 
   def test_pthp_othercoils_rb
     result = sim_test('pthp_othercoils.rb')
   end
 
-  # TODO: To be added in the next official release after: 3.1.0
-  # def test_pthp_othercoils_osm
-  # result = sim_test('pthp_othercoils.osm')
-  # end
+  def test_pthp_othercoils_osm
+    result = sim_test('pthp_othercoils.osm')
+  end
 
   def test_airloop_avms_rb
     result = sim_test('airloop_avms.rb')
