d7d48535c2ad268b80aa497a97b98c3a35a3c1fd
Joe Robertson <joseph.robertson@nrel.gov>
2021-06-07 14:13:41 -0700

    Update model tests file.

--- model_tests.rb
+++ model_tests.rb
@@ -1047,6 +1047,15 @@ class ModelTests < Minitest::Test
     result = sim_test('unitary_vav_bypass_plenum.osm')
   end
 
+  def test_unitary_vav_bypass_coiltypes_rb
+    result = sim_test('unitary_vav_bypass_coiltypes.rb')
+  end
+
+  # TODO: To be added in the next official release after: 3.2.0
+  # def test_unitary_vav_bypass_coiltypes_osm
+    # result = sim_test('unitary_vav_bypass_coiltypes.osm')
+  # end
+
   def test_unitary_systems_airloop_and_zonehvac_rb
     result = sim_test('unitary_systems_airloop_and_zonehvac.rb')
   end
