152b03f8050ca34452aa2b9120f81ba2da2e7280
Joe Robertson <joseph.robertson@nrel.gov>
2022-05-26 14:21:01 -0700

    Files for python plugin test.

---
+++ model/simulationtests/python_plugin.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'openstudio'
+require_relative 'lib/baseline_model'
+
+model = BaselineModel.new
+
+# make a 2 story, 100m X 50m, 10 zone core/perimeter building
+model.add_geometry({ 'length' => 100,
+                     'width' => 50,
+                     'num_floors' => 2,
+                     'floor_to_floor_height' => 4,
+                     'plenum_height' => 1,
+                     'perimeter_zone_depth' => 3 })
+
+# add windows at a 40% window-to-wall ratio
+model.add_windows({ 'wwr' => 0.4,
+                    'offset' => 1,
+                    'application_type' => 'Above Floor' })
+
+# add ASHRAE System type 01, PTAC, Residential
+model.add_hvac({ 'ashrae_sys_num' => '01' })
+
+# add thermostats
+model.add_thermostats({ 'heating_setpoint' => 24,
+                        'cooling_setpoint' => 28 })
+
+# assign constructions from a local library to the walls/windows/etc. in the model
+model.set_constructions
+
+# set whole building space type; simplified 90.1-2004 Large Office Whole Building
+model.set_space_type
+
+# add design days to the model (Chicago)
+model.add_design_days
+
+# create the external file object
+file_name = File.join(File.dirname(__FILE__), 'lib/PythonPluginThermochromicWindow.py')
+file_name = File.realpath(file_name)
+external_file = OpenStudio::Model::ExternalFile.getExternalFile(model, file_name)
+external_file = external_file.get
+
+# create the python plugin instance object
+python_plugin_instance = OpenStudio::Model::PythonPluginInstance.new(external_file, "ZN_1_wall_south_Window_1_Control")
+
+# save the OpenStudio model (.osm)
+model.save_openstudio_osm({ 'osm_save_directory' => Dir.pwd, 'osm_name' => 'in.osm' })
