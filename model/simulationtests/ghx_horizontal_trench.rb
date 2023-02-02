56010b275a1604ef15c323e76f5aa84fe595f5cc
Julien Marrec <julien.marrec@gmail.com>
2023-02-02 16:42:36 +0100

    NOTE: IT HAS SWITCHES, REMOVE. Modify test to use a LoadProfile:Plant instead.... make it pass with 3.5.1.

--- model/simulationtests/ghx_horizontal_trench.rb
+++ model/simulationtests/ghx_horizontal_trench.rb
@@ -5,36 +5,20 @@ require_relative 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# make a 1 story, 100m X 50m, 1 zone building
-model.add_geometry({ 'length' => 100,
-                     'width' => 50,
-                     'num_floors' => 1,
-                     'floor_to_floor_height' => 4,
-                     'plenum_height' => 0,
-                     'perimeter_zone_depth' => 0 })
-
-# add windows at a 40% window-to-wall ratio
-model.add_windows({ 'wwr' => 0.4,
-                    'offset' => 1,
-                    'application_type' => 'Above Floor' })
-
-# add thermostats
-model.add_thermostats({ 'heating_setpoint' => 24,
-                        'cooling_setpoint' => 28 })
-
-# assign constructions from a local library to the walls/windows/etc. in the model
-model.set_constructions
-
-# set whole building space type; simplified 90.1-2004 Large Office Whole Building
-model.set_space_type
+# No zones, just a LoadProfile:Plant
+# This is unfortunate but the only E+ example file that uses it is PlantHorizontalGroundHX.idf
+# and it has just a LoadProfile:Plant, and nothing autosized
+# We couldn't get this file to not throw a Plant run away temperature so we're
+# matching the E+ test instead
 
 # add design days to the model (Chicago)
 model.add_design_days
 
-# In order to produce more consistent results between different runs,
-# we sort the zones by names
-zones = model.getThermalZones.sort_by { |z| z.name.to_s }
-zone = zones[0]
+USE_KUSUDA = true
+USE_XING = false
+USE_PIPE_INDOOR = false
+
+raise "Cannot use XING on 3.5.1 and below" if USE_XING and Gem::Version.new(OpenStudio.openStudioVersion) <= Gem::Version.new('3.5.1')
 
 # Add a hot water plant to supply the water to air heat pump
 # This could be baked into HVAC templates in the future
@@ -52,35 +36,76 @@ condenserWaterInletNode = condenserWaterPlant.supplyInletNode
 pump = OpenStudio::Model::PumpVariableSpeed.new(model)
 pump.addToNode(condenserWaterInletNode)
 
-kusudaAchenbach = OpenStudio::Model::SiteGroundTemperatureUndisturbedKusudaAchenbach.new(model)
-kusudaAchenbach.setSoilThermalConductivity(1.08)
-kusudaAchenbach.setSoilDensity(962)
-kusudaAchenbach.setSoilSpecificHeat(2576)
-kusudaAchenbach.setAverageSoilSurfaceTemperature(15.5)
-kusudaAchenbach.setAverageAmplitudeofSurfaceTemperature(12.8)
-kusudaAchenbach.setPhaseShiftofMinimumSurfaceTemperature(17.3)
+if USE_KUSUDA
+  hGroundHX1 = nil
+  if Gem::Version.new(OpenStudio.openStudioVersion) > Gem::Version.new('3.5.1')
+    kusudaAchenbach = OpenStudio::Model::SiteGroundTemperatureUndisturbedKusudaAchenbach.new(model)
+    kusudaAchenbach.setSoilThermalConductivity(1.08)
+    kusudaAchenbach.setSoilDensity(962)
+    kusudaAchenbach.setSoilSpecificHeat(2576)
+    kusudaAchenbach.setAverageSoilSurfaceTemperature(15.5)
+    kusudaAchenbach.setAverageAmplitudeofSurfaceTemperature(12.8)
+    kusudaAchenbach.setPhaseShiftofMinimumSurfaceTemperature(17.3)
+    hGroundHX1 = OpenStudio::Model::GroundHeatExchangerHorizontalTrench.new(model, kusudaAchenbach)
+  else
+    hGroundHX1 = OpenStudio::Model::GroundHeatExchangerHorizontalTrench.new(model)
+    hGroundHX1.setSoilThermalConductivity(1.08)
+    hGroundHX1.setSoilDensity(962)
+    hGroundHX1.setSoilSpecificHeat(2576)
+    hGroundHX1.setKusudaAchenbachAverageSurfaceTemperature(15.5)
+    hGroundHX1.setKusudaAchenbachAverageAmplitudeofSurfaceTemperature(12.8)
+    hGroundHX1.setKusudaAchenbachPhaseShiftofMinimumSurfaceTemperature(17.3)
+  end
+  condenserWaterPlant.addSupplyBranchForComponent(hGroundHX1)
+end
 
-xing = OpenStudio::Model::SiteGroundTemperatureUndisturbedXing.new(model)
-xing.setSoilThermalConductivity(1.08)
-xing.setSoilDensity(962)
-xing.setSoilSpecificHeat(2576)
-xing.setAverageSoilSurfaceTemperature(11.1)
-xing.setSoilSurfaceTemperatureAmplitude1(13.4)
-xing.setSoilSurfaceTemperatureAmplitude2(0.7)
-xing.setPhaseShiftofTemperatureAmplitude1(25)
-xing.setPhaseShiftofTemperatureAmplitude2(30)
+if USE_XING
+  xing = OpenStudio::Model::SiteGroundTemperatureUndisturbedXing.new(model)
+  xing.setSoilThermalConductivity(1.08)
+  xing.setSoilDensity(962)
+  xing.setSoilSpecificHeat(2576)
+  xing.setAverageSoilSurfaceTemperature(11.1)
+  xing.setSoilSurfaceTemperatureAmplitude1(13.4)
+  xing.setSoilSurfaceTemperatureAmplitude2(0.7)
+  xing.setPhaseShiftofTemperatureAmplitude1(25)
+  xing.setPhaseShiftofTemperatureAmplitude2(30)
+  hGroundHX2 = OpenStudio::Model::GroundHeatExchangerHorizontalTrench.new(model, xing)
+  condenserWaterPlant.addSupplyBranchForComponent(hGroundHX2)
+end
 
-hGroundHX1 = OpenStudio::Model::GroundHeatExchangerHorizontalTrench.new(model, kusudaAchenbach)
-hGroundHX2 = OpenStudio::Model::GroundHeatExchangerHorizontalTrench.new(model, xing)
+if USE_PIPE_INDOOR
+  pipe_mat = OpenStudio::Model::StandardOpaqueMaterial.new(model, 'Smooth', 3.00E-03, 45.31, 7833.0, 500.0)
+  pipe_mat.setThermalAbsorptance(OpenStudio::OptionalDouble.new(0.9))
+  pipe_mat.setSolarAbsorptance(OpenStudio::OptionalDouble.new(0.5))
+  pipe_mat.setVisibleAbsorptance(OpenStudio::OptionalDouble.new(0.5))
+  pipe_const = OpenStudio::Model::Construction.new(model)
+  pipe_const.insertLayer(0, pipe_mat)
 
-condenserWaterPlant.addSupplyBranchForComponent(hGroundHX1)
-condenserWaterPlant.addSupplyBranchForComponent(hGroundHX2)
+  pipe = OpenStudio::Model::PipeIndoor.new(model)
+  pipe.setAmbientTemperatureZone(zone)
+  pipe.setConstruction(pipe_const)
+  condenserWaterPlant.addSupplyBranchForComponent(pipe)
 
-pipe = OpenStudio::Model::PipeAdiabatic.new(model)
-condenserWaterPlant.addSupplyBranchForComponent(pipe)
+  pipe2 = OpenStudio::Model::PipeIndoor.new(model)
+  pipe2.setAmbientTemperatureZone(zone)
+  pipe2.setConstruction(pipe_const)
+  pipe2.addToNode(condenserWaterOutletNode)
 
-pipe2 = OpenStudio::Model::PipeAdiabatic.new(model)
-pipe2.addToNode(condenserWaterOutletNode)
+  pipe3 = OpenStudio::Model::PipeIndoor.new(model)
+  pipe3.setAmbientTemperatureZone(zone)
+  pipe3.setConstruction(pipe_const)
+  pipe3.addToNode(condenserWaterPlant.demandInletNode)
+
+  pipe4 = OpenStudio::Model::PipeIndoor.new(model)
+  pipe4.setAmbientTemperatureZone(zone)
+  pipe4.setConstruction(pipe_const)
+  pipe4.addToNode(condenserWaterPlant.demandOutletNode)
+
+  pipe5 = OpenStudio::Model::PipeIndoor.new(model)
+  pipe5.setAmbientTemperatureZone(zone)
+  pipe5.setConstruction(pipe_const)
+  condenserWaterPlant.addDemandBranchForComponent(pipe5)
+end
 
 ## Make a condenser Water temperature schedule
 
@@ -108,35 +133,37 @@ condenserWaterTempSchedule.defaultDaySchedule.addValue(osTime, 24)
 condenserWaterSPM = OpenStudio::Model::SetpointManagerScheduled.new(model, condenserWaterTempSchedule)
 condenserWaterSPM.addToNode(condenserWaterOutletNode)
 
-# add water to air heat pump to next available zone
-fanPowerFtSpeedCurve = OpenStudio::Model::CurveExponent.new(model)
-fanPowerFtSpeedCurve.setCoefficient1Constant(0.0)
-fanPowerFtSpeedCurve.setCoefficient2Constant(1.0)
-fanPowerFtSpeedCurve.setCoefficient3Constant(3.0)
-fanPowerFtSpeedCurve.setMinimumValueofx(0.0)
-fanPowerFtSpeedCurve.setMaximumValueofx(1.5)
-fanPowerFtSpeedCurve.setMinimumCurveOutput(0.01)
-fanPowerFtSpeedCurve.setMaximumCurveOutput(1.5)
+condenserWaterPlant.setMaximumLoopTemperature(80.0)
+condenserWaterPlant.setMaximumLoopFlowRate(0.004)
+condenserWaterPlant.setFluidType("PropyleneGlycol")
+condenserWaterPlant.setGlycolConcentration(70)
 
-fanEfficiencyFtSpeedCurve = OpenStudio::Model::CurveCubic.new(model)
-fanEfficiencyFtSpeedCurve.setCoefficient1Constant(0.33856828)
-fanEfficiencyFtSpeedCurve.setCoefficient2x(1.72644131)
-fanEfficiencyFtSpeedCurve.setCoefficient3xPOW2(-1.49280132)
-fanEfficiencyFtSpeedCurve.setCoefficient4xPOW3(0.42776208)
-fanEfficiencyFtSpeedCurve.setMinimumValueofx(0.5)
-fanEfficiencyFtSpeedCurve.setMaximumValueofx(1.5)
-fanEfficiencyFtSpeedCurve.setMinimumCurveOutput(0.3)
-fanEfficiencyFtSpeedCurve.setMaximumCurveOutput(1.0)
+pump.setRatedFlowRate(0.004)
+pump.setRatedPumpHead(5000.0)
+pump.setRatedPowerConsumption(25.0)
+pump.setFractionofMotorInefficienciestoFluidStream(0.0)
+pump.setPumpControlType('Intermittent')
 
-supplyFan = OpenStudio::Model::FanOnOff.new(model, model.alwaysOnDiscreteSchedule, fanPowerFtSpeedCurve, fanEfficiencyFtSpeedCurve)
-wahpDXHC = OpenStudio::Model::CoilHeatingWaterToAirHeatPumpEquationFit.new(model)
-wahpDXCC = OpenStudio::Model::CoilCoolingWaterToAirHeatPumpEquationFit.new(model)
-supplementalHC = OpenStudio::Model::CoilHeatingElectric.new(model, model.alwaysOnDiscreteSchedule)
-wtahp = OpenStudio::Model::ZoneHVACWaterToAirHeatPump.new(model, model.alwaysOnDiscreteSchedule, supplyFan, wahpDXHC, wahpDXCC, supplementalHC)
-wtahp.addToThermalZone(zone)
 
-condenserWaterPlant.addDemandBranchForComponent(wahpDXHC)
-condenserWaterPlant.addDemandBranchForComponent(wahpDXCC)
+# Load Profile
+loadProfile = OpenStudio::Model::LoadProfilePlant.new(model)
+loadProfile.setPeakFlowRate(0.004)
+
+flowFracSchedule = OpenStudio::Model::ScheduleConstant.new(model)
+flowFracSchedule.setName('FlowFracSchedule')
+flowFracSchedule.setValue(1.0)
+loadProfile.setFlowRateFractionSchedule(flowFracSchedule)
+
+loadSchedule = OpenStudio::Model::ScheduleRuleset.new(model)
+loadSchedule.setName('LoadSchedule')
+loadSchedule.defaultDaySchedule.addValue(osTime, 2000.0)
+loadSchedule_may_to_sept_rule = OpenStudio::Model::ScheduleRule.new(loadSchedule)
+loadSchedule_may_to_sept_rule.setStartDate(OpenStudio::Date.new('May'.to_MonthOfYear, 1))
+loadSchedule_may_to_sept_rule.setEndDate(OpenStudio::Date.new('September'.to_MonthOfYear, 30))
+loadSchedule_may_to_sept_rule.daySchedule.addValue(osTime, -3000.0)
+loadProfile.setLoadSchedule(loadSchedule)
+
+condenserWaterPlant.addDemandBranchForComponent(loadProfile)
 
 # save the OpenStudio model (.osm)
 model.save_openstudio_osm({ 'osm_save_directory' => Dir.pwd,
