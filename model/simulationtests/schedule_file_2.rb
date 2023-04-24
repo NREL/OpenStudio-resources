6585957ae62eb4500aba0f7c63f00d611a251189
Joe Robertson <joseph.robertson@nrel.gov>
2023-04-24 09:48:56 -0700

    Use one zone and ideal air loads.

--- model/simulationtests/schedule_file_2.rb
+++ model/simulationtests/schedule_file_2.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
+# This test aims to test the new ScheduleFile ctor that takes a direct path and does not make a copy to the companion directory. Cf NREL/OpenStudio#4857
+
 require 'openstudio'
 require_relative 'lib/baseline_model'
 
 model = BaselineModel.new
 
-# make a 2 story, 100m X 50m, 10 zone core/perimeter building
+# make a 1 story, 100m X 50m, 1 zone building
 model.add_geometry({ 'length' => 100,
                      'width' => 50,
-                     'num_floors' => 2,
+                     'num_floors' => 1,
                      'floor_to_floor_height' => 4,
-                     'plenum_height' => 1,
-                     'perimeter_zone_depth' => 3 })
+                     'plenum_height' => 0,
+                     'perimeter_zone_depth' => 0 })
 
 # add windows at a 40% window-to-wall ratio
 model.add_windows({ 'wwr' => 0.4,
                     'offset' => 1,
                     'application_type' => 'Above Floor' })
 
-# add ASHRAE System type 03, PSZ-AC
-model.add_hvac({ 'ashrae_sys_num' => '03' })
-
 # add thermostats
-model.add_thermostats({ 'heating_setpoint' => 24,
-                        'cooling_setpoint' => 28 })
+model.add_thermostats({ 'heating_setpoint' => 19,
+                        'cooling_setpoint' => 26 })
 
 # assign constructions from a local library to the walls/windows/etc. in the model
 model.set_constructions
@@ -33,6 +32,14 @@ model.set_space_type
 
 # add design days to the model (Chicago)
 model.add_design_days
+
+# In order to produce more consistent results between different runs,
+# we sort the zones by names
+# (There's only one here, but just in case this would be copy pasted somewhere
+# else...)
+zones = model.getThermalZones.sort_by { |z| z.name.to_s }
+z = zones[0]
+z.setUseIdealAirLoads(true)
 
 ###############################################################################
 
